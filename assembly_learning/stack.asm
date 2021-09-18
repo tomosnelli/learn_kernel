b9e298265e6307cef1b6670729670f71c412545f
Tomo Kaneko <tkaneko@my.bcit.ca>
2021-09-18 11:48:02 -0700

    Made it work, see push/pop process

--- assembly_learning/stack.asm
+++ assembly_learning/stack.asm
@@ -1,23 +1,39 @@
-; didn't add section .data... probably will work??
+section .data
+	; this is 2 bytes
+	digit db 0,10
+
 section .text
 	global _start
 
 _start:
-	push 3
-	call _seeStack
-	push 2
-	call _seeStack
-	push 1
-	call _seeStack
-	push 0
-	call _seeStack
+	;mov rax, 7
+	;call _printRAXDigit
+	; the above code causes a coredump for some reason. I need to learn more
 
-_seeStack:
-	mov r10, [rsp]
+	push 4
+	push 3
+	push 2
+	push 1
+
+	pop rax
+	call _printRAXDigit
+	pop rax
+	call _printRAXDigit
+	pop rax
+	call _printRAXDigit
+	pop rax
+	call _printRAXDigit
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
+
+_printRAXDigit:
+	add rax, 48		; so rax now has and ASCII code "0"
+	mov [digit], al ; move the lower bits for data into what digit is pointing to 10 is newline and no problem
 	mov rax, 1
 	mov rdi, 1
-	mov rsi, r10
-	mov rdx, 4
+	mov rsi, digit
+	mov rdx, 2
 	syscall
 	ret
-	
