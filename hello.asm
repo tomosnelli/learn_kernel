bd35f6ed1624156e5336d417f959a29390195288
Tomo Kaneko <tkaneko@my.bcit.ca>
2021-09-17 20:07:19 -0700

    Make nams program to take user input

--- hello.asm
+++ hello.asm
@@ -1,14 +1,50 @@
 section .data
-	text db "Hello, World!", 10
+	text1 db "ENTER NAME: "
+	text2 db "Hello, "
+
+section .bss
+	name resb 16 ; resb is reserve bytes. this will reserve 16 bytes
+
 section .text
 	global _start
 _start:
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, text
-	mov rdx, 14
-	syscall
+	call _printText1
+	call _getName
+	call _printText2
+	call _printName
 
 	mov rax, 60
 	mov rdi, 0
 	syscall
+
+_getName:
+	mov rax, 0
+	mov rdi, 0
+	mov rsi, name
+	mov rdx, 16
+	syscall
+	ret
+
+_printText1:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, text1
+	mov rdx, 12
+	syscall 
+	ret
+
+_printText2:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, text2
+	mov rdx, 7
+	syscall
+	ret
+
+_printName:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, name
+	mov rdx, 16
+	syscall
+	ret
