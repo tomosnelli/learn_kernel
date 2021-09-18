62b0d0428cdca47c795673b23eba17b38ed5c292
Tomo Kaneko <tkaneko@my.bcit.ca>
2021-09-18 10:24:42 -0700

    Organize file structure

---
+++ assembly_learning/test_math.asm
@@ -0,0 +1,24 @@
+section .data
+	; this is 2 bytes
+	digit db 0,10
+
+section .text
+	global _start
+
+_start:
+	mov rax, 7
+	call _printRAXDigit
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
+
+_printRAXDigit:
+	add rax, 48		; so rax now has and ASCII code "0"
+	mov [digit], al ; move the lower bits for data into what digit is pointing to 10 is newline and no problem
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, digit
+	mov rdx, 2
+	syscall
+	ret
